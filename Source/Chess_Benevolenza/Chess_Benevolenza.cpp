1745218b18151ef6d16c4283bb471d090752b9ba
marixmas <108756623+marixmas@users.noreply.github.com>
2024-04-13 23:23:03 +0200

    upload da cartella di onedrive

---
+++ Source/Chess_Benevolenza/Chess_Benevolenza.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Chess_Benevolenza.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chess_Benevolenza, "Chess_Benevolenza" );
---
+++ Source/Chess_Benevolenza/Chess_BenevolenzaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Chess_BenevolenzaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_BENEVOLENZA_API AChess_BenevolenzaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
