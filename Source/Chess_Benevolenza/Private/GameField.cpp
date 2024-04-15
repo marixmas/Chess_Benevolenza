af4a979e4bcbfa6e91d1a0599b8806d85b1afeaf
marixmas <108756623+marixmas@users.noreply.github.com>
2024-04-15 18:55:43 +0200

    aggiunto gamefield

---
+++ Source/Chess_Benevolenza/Private/GameField.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameField.h"
+
+// Sets default values
+AGameField::AGameField()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGameField::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGameField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
