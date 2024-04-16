168609d7df02c3351033e849ee62703b790aa40e
marixmas <108756623+marixmas@users.noreply.github.com>
2024-04-16 09:43:30 +0200

    classi specifiche delle pedine

---
+++ Source/Chess_Benevolenza/Public/Chess_Piece_Bishop.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Chess_Piece.h"
+#include "Chess_Piece_Bishop.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_BENEVOLENZA_API AChess_Piece_Bishop : public AChess_Piece
+{
+	GENERATED_BODY()
+	
+};
