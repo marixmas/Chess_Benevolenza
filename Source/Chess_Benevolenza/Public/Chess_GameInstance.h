274c21b0dce35d081634ccec44e0976226fd0e59
marixmas <108756623+marixmas@users.noreply.github.com>
2024-04-16 09:52:22 +0200

    aggiunte le restanti classi (vuote)

---
+++ Source/Chess_Benevolenza/Public/Chess_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Chess_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESS_BENEVOLENZA_API UChess_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Chess_Benevolenza/Public/Chess_PlayerInterface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Chess_PlayerInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UChess_PlayerInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class CHESS_BENEVOLENZA_API IChess_PlayerInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
